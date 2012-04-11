1da93e226f5fd7fadf978fd2407d492f4de16145
kohei sakai <k.sakai@math.kyoto-u.ac.jp>
2012-04-11 17:03:45 +0900

    This script is a tool for ldapsearch.
    Most of OA_member cannot use ldap_toos.
    Actually, ldapsearch has many options and they are complicate.
    However, using this script, just typing filters as a arguments(e.g. In your cmdline,$ "this script" cn=kohei uidNumber=100),
    admin users can access ldapsearver and their entries.
    
    Caution:
    Now, this script supports only 'and' search filters.
    
    Example:
    $ "this script" uid=k.sakai uidNumber=3152

---
+++ OA_ldapsearch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+### This script is a tool for ldapsearch.
+### 
+### Most of OA_member cannot use ldap_toos.
+### Actually, ldapsearch has many options and they are complicate.
+### However, using this script, just typing filters as a arguments(e.g. In your cmdline,$ "this script" cn=kohei uidNumber=100),
+### admin users can access ldapsearver and their entries.
+### 
+### Caution:
+### Now, this script supports only 'and' search filters.
+### 
+### Example:
+### $ "this script" uid=k.sakai uidNumber=3152
+### 
+
+if [ $UID != 0 ] ; then
+	echo "For this script, you have to use 'sudo' command"
+	exit 0
+fi
+
+IFS=:
+PASS=`cat /etc/ldap.secret`
+SEARCH_FILTER="(&(`echo "$*" | sed -e 's/:/\)\(/g'`))"
+echo "$SEARCH_FILTER"
+echo "ldapsearch -D \"cn=Manager,dc=math,dc=kyoto-u,dc=ac,dc=jp\" -w \$PASS -b \"dc=math,dc=kyoto-u,dc=ac,dc=jp\" \"$SEARCH_FILTER\""
+
+echo "Search Results"
+ldapsearch -D "cn=Manager,dc=math,dc=kyoto-u,dc=ac,dc=jp" -w $PASS -b "dc=math,dc=kyoto-u,dc=ac,dc=jp" "$SEARCH_FILTER" -v
+
